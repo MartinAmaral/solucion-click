660779731fd3dc3a85d2f111f42789b7bd6b403e
Martín Amaral <martin.amaral.g96@gmail.com>
2023-08-05 21:19:56 -0300

    Solucion Final

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
 
+var input = document.getElementsByTagName("input");
+input[0].addEventListener("click", function(e){
+    alert('Hola!');
+    e.stopPropagation();
+});
+
 var div = document.getElementsByTagName("div");
 div[0].addEventListener("click", function(){ 
     alert('Hola! Soy el div');
